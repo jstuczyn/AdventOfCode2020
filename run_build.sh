2b3c8ba58d9e82cdd7c78f441dba24be4079e8a7
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-12-05 11:22:23 +0000

    Added clippy

--- run_build.sh
+++ run_build.sh
@@ -3,6 +3,7 @@
 for i in {1..25}; do
     if [ -d "day$i" ]; then
             cargo fmt --manifest-path=day"$i"/Cargo.toml --verbose --all -- --check
+            cargo clippy --manifest-path=day"$i"/Cargo.toml --verbose --all -- -D warnings
             cargo build --manifest-path=day"$i"/Cargo.toml --verbose --all
             cargo test --manifest-path=day"$i"/Cargo.toml --verbose --all
     else
