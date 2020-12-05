1c4e114b5ab9a09f679bd21574667a55b02bf74f
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-12-05 11:29:50 +0000

    Hopefully failing travis on failed checks

--- run_build.sh
+++ run_build.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+RET=0
 for i in {1..25}; do
     if [ -d "day$i" ]; then
-            cargo fmt --manifest-path=day"$i"/Cargo.toml --verbose --all -- --check
-            cargo clippy --manifest-path=day"$i"/Cargo.toml --verbose --all -- -D warnings
-            cargo build --manifest-path=day"$i"/Cargo.toml --verbose --all
-            cargo test --manifest-path=day"$i"/Cargo.toml --verbose --all
+            cargo fmt --manifest-path=day"$i"/Cargo.toml --verbose --all -- --check || RET=1;
+            cargo clippy --manifest-path=day"$i"/Cargo.toml --verbose --all -- -D warnings || RET=1;
+            cargo build --manifest-path=day"$i"/Cargo.toml --verbose --all || RET=1;
+            cargo test --manifest-path=day"$i"/Cargo.toml --verbose --all || RET=1;
     else
         echo "No solution for day $i found"
     fi 
 done
+
+exit $RET
