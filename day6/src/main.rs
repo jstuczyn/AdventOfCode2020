1c33ab1150d34f65a035f6c69381dbf4cce342f4
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-12-06 10:28:58 +0000

    part 2 answer + sample tests

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -12,27 +12,45 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::collections::HashSet;
+use std::collections::HashMap;
 use utils::input_read;
 
-struct Group(HashSet<char>);
+struct Group {
+    size: usize,
+    answers: HashMap<char, usize>,
+}
 
 impl<'a> From<&'a String> for Group {
-    fn from(answers: &'a String) -> Self {
-        let mut unique_answers = HashSet::new();
-        answers
+    fn from(answers_raw: &'a String) -> Self {
+        let mut answers = HashMap::new();
+
+        let mut size = 0;
+
+        answers_raw
             .split_ascii_whitespace()
-            .flat_map(|answer| answer.chars())
+            .flat_map(|answer| {
+                size += 1;
+                answer.chars()
+            })
             .for_each(|char| {
-                unique_answers.insert(char);
+                let count = answers.entry(char).or_insert(0);
+                *count += 1;
             });
-        Group(unique_answers)
+
+        Group { size, answers }
     }
 }
 
 impl Group {
     fn len(&self) -> usize {
-        self.0.len()
+        self.answers.len()
+    }
+
+    fn all_yes(&self) -> usize {
+        self.answers
+            .iter()
+            .filter(|(_, &count)| count == self.size)
+            .count()
     }
 }
 
@@ -43,8 +61,11 @@ fn part1(input: &[String]) -> usize {
         .sum()
 }
 
-fn part2(input: &[String]) -> Option<usize> {
-    None
+fn part2(input: &[String]) -> usize {
+    input
+        .iter()
+        .map(|group_answers| Group::from(group_answers).all_yes())
+        .sum()
 }
 
 #[cfg(not(tarpaulin))]
@@ -53,6 +74,59 @@ fn main() {
     let part1_result = part1(&input);
     println!("Part 1 result is {}", part1_result);
 
-    // let part2_result = part2(&input);
-    // println!("Part 2 result is {}", part2_result);
+    let part2_result = part2(&input);
+    println!("Part 2 result is {}", part2_result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_sample_input() {
+        let input = vec![
+            "abc".to_string(),
+            "a
+b
+c"
+            .to_string(),
+            "ab
+ac"
+            .to_string(),
+            "a
+a
+a
+a"
+            .to_string(),
+            "b".to_string(),
+        ];
+
+        let expected = 11;
+
+        assert_eq!(expected, part1(&input))
+    }
+
+    #[test]
+    fn part2_sample_input() {
+        let input = vec![
+            "abc".to_string(),
+            "a
+b
+c"
+            .to_string(),
+            "ab
+ac"
+            .to_string(),
+            "a
+a
+a
+a"
+            .to_string(),
+            "b".to_string(),
+        ];
+
+        let expected = 6;
+
+        assert_eq!(expected, part2(&input))
+    }
 }
