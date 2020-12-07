607d8d37a25c96c5997a01121864e9953b6c3bb1
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-12-07 20:57:48 +0000

    part 1 finished

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -12,6 +12,223 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::cell::RefCell;
+use std::collections::{HashMap, HashSet};
+use std::fmt::{self, Debug, Formatter};
+use std::rc::Rc;
+use utils::input_read;
+
+const EMPTY_BAG: &str = "no other";
+const TARGET_BAG: &str = "shiny gold";
+
+struct Traversal<'a> {
+    visited: HashSet<String>,
+    graph: &'a BagGraph,
+}
+
+impl<'a> Traversal<'a> {
+    fn new(graph: &'a BagGraph) -> Self {
+        Traversal {
+            visited: HashSet::new(),
+            // direct_parents: Vec::new(),
+            graph,
+        }
+    }
+
+    fn visit_entry(&mut self, entry: &Rc<RefCell<Bag>>) {
+        for parent in &entry.borrow().parents {
+            if self.visited.insert(parent.borrow().name.clone()) {
+                self.visit_entry(parent);
+            }
+        }
+    }
+
+    fn get_unique_parents(&mut self, bag_name: &str) -> HashSet<String> {
+        let entry = self.graph.nodes.get(bag_name).unwrap();
+
+        for parent in &entry.borrow().parents {
+            self.visited.insert(parent.borrow().name.clone());
+            self.visit_entry(parent)
+        }
+
+        self.visited.clone()
+    }
+}
+
+#[derive(Default)]
+struct BagGraph {
+    nodes: HashMap<String, Rc<RefCell<Bag>>>,
+}
+
+impl BagGraph {
+    fn new() -> Self {
+        Default::default()
+    }
+
+    fn insert_rule(&mut self, bag_name: String, inner: Vec<BagInnerRaw>) {
+        let mut inner_bags = Vec::with_capacity(inner.len());
+        let mut inserted_inner_bags = Vec::with_capacity(inner.len());
+
+        for (inner_bag_count, inner_bag_name) in inner {
+            // inner rcs
+            let node = self
+                .nodes
+                .entry(inner_bag_name.clone())
+                .or_insert_with(|| Rc::new(RefCell::new(Bag::new(inner_bag_name))));
+
+            inner_bags.push(BagInner {
+                count: inner_bag_count,
+                bag: Rc::clone(&node),
+            });
+
+            inserted_inner_bags.push(Rc::clone(&node));
+        }
+
+        let parent = self
+            .nodes
+            .entry(bag_name.clone())
+            .or_insert_with(|| Rc::new(RefCell::new(Bag::new(bag_name.clone()))));
+
+        // it can't have been set before
+        debug_assert!(parent.borrow().inner.is_empty());
+        parent.borrow_mut().set_inner(inner_bags);
+
+        for inner_bag in inserted_inner_bags {
+            inner_bag.borrow_mut().add_parent(Rc::clone(&parent))
+        }
+    }
+}
+
+impl Debug for BagGraph {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        writeln!(f, "BAG GRAPH")?;
+        for (name, children) in self.nodes.iter() {
+            writeln!(f, "{}:", name)?;
+            for child in &children.borrow().inner {
+                writeln!(f, "\t{} x {}", child.count, child.bag.borrow().name)?;
+            }
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+struct Bag {
+    // each bag has a name/colour
+    name: String,
+    // and can have multiple other bags inside it
+    inner: Vec<BagInner>,
+    // and can have multiple parents
+    parents: Vec<Rc<RefCell<Bag>>>,
+}
+
+impl Bag {
+    fn new(name: String) -> Self {
+        Bag {
+            name,
+            inner: Vec::new(),
+            parents: Vec::new(),
+        }
+    }
+
+    fn add_parent(&mut self, parent: Rc<RefCell<Bag>>) {
+        self.parents.push(parent)
+    }
+
+    fn set_inner(&mut self, inner: Vec<BagInner>) {
+        self.inner = inner
+    }
+}
+
+type BagInnerRaw = (usize, String);
+
+struct BagInner {
+    count: usize,
+    bag: Rc<RefCell<Bag>>,
+}
+
+impl Debug for BagInner {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{} x {}", self.count, self.bag.borrow().name)
+    }
+}
+
+fn into_count_and_name(raw_bag: &str) -> Option<(usize, String)> {
+    if raw_bag.starts_with(EMPTY_BAG) {
+        return None;
+    }
+    let whitespace_split: Vec<_> = raw_bag.split_ascii_whitespace().collect();
+    // we must at least have a count and name
+    assert!(whitespace_split.len() >= 2);
+    let count = whitespace_split[0]
+        .parse()
+        .expect("failed to parse bag count!");
+
+    // final chunk is word 'bag' or 'bags' and we don't care about it
+    let name = whitespace_split[1..whitespace_split.len() - 1].join(" ");
+    Some((count, name))
+}
+
+fn parse_rule(rule: &String) -> (String, Vec<BagInnerRaw>) {
+    let split: Vec<_> = rule.split("contain").collect();
+    let bag_name = split[0].trim().trim_end_matches(" bags").to_owned();
+
+    let inner_bags = split[1].split(|c| c == ',' || c == '.');
+    let raw_inner = inner_bags
+        .map(str::trim)
+        .filter(|bag| !bag.is_empty())
+        .map(into_count_and_name)
+        .filter_map(|entry| entry)
+        .collect();
+
+    (bag_name, raw_inner)
+}
+
+fn part1(input: &[String]) -> usize {
+    let mut graph = BagGraph::new();
+    input
+        .iter()
+        .map(parse_rule)
+        .for_each(|rule| graph.insert_rule(rule.0, rule.1));
+
+    Traversal::new(&graph).get_unique_parents(TARGET_BAG).len()
+}
+
+// fn part2(input: &[String]) -> usize {
+//     0
+// }
+
+#[cfg(not(tarpaulin))]
 fn main() {
-    println!("Hello, world!");
+    let input = input_read::read_line_input("input").expect("failed to read input file");
+
+    let part1_result = part1(&input);
+    println!("Part 1 result is {}", part1_result);
+    //
+    // let part2_result = part2(&input);
+    // println!("Part 2 result is {}", part2_result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample_part1_input() {
+        let input = vec![
+            "light red bags contain 1 bright white bag, 2 muted yellow bags.".to_string(),
+            "dark orange bags contain 3 bright white bags, 4 muted yellow bags.".to_string(),
+            "bright white bags contain 1 shiny gold bag.".to_string(),
+            "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.".to_string(),
+            "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.".to_string(),
+            "dark olive bags contain 3 faded blue bags, 4 dotted black bags.".to_string(),
+            "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.".to_string(),
+            "faded blue bags contain no other bags.".to_string(),
+            "dotted black bags contain no other bags.".to_string(),
+        ];
+
+        let expected = 4;
+
+        assert_eq!(expected, part1(&input));
+    }
 }
