d8f21e1b34a5af895f7f6d112f369b5ed7e20fb6
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-12-08 11:38:32 +0000

    part2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -14,16 +14,28 @@
 
 use std::collections::HashSet;
 use std::convert::TryFrom;
+use std::fmt::{self, Debug, Formatter};
 use utils::input_read;
 
 const ACC_OPCODE: &str = "acc";
 const JMP_OPCODE: &str = "jmp";
 const NOP_OPCODE: &str = "nop";
 
+#[derive(Clone, Eq, PartialEq)]
 enum Opcode {
     Acc,
     Jump,
     Nop,
+}
+
+impl Debug for Opcode {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Opcode::Acc => write!(f, "{}", ACC_OPCODE),
+            Opcode::Jump => write!(f, "{}", JMP_OPCODE),
+            Opcode::Nop => write!(f, "{}", NOP_OPCODE),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -61,6 +73,12 @@ struct Instruction {
     operand: isize,
 }
 
+impl Debug for Instruction {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?} {}", self.opcode, self.operand)
+    }
+}
+
 impl<'a> TryFrom<&'a String> for Instruction {
     type Error = InvalidInstruction;
 
@@ -82,52 +100,88 @@ impl<'a> TryFrom<&'a String> for Instruction {
     }
 }
 
+enum ExecutionError {
+    LoopDetected(isize),
+}
+
+impl ExecutionError {
+    fn into_inner(self) -> isize {
+        match self {
+            ExecutionError::LoopDetected(final_accumulator) => final_accumulator,
+        }
+    }
+}
+
 struct Computer {
     // currently there's only a single register, so until requirements change, keep it like this
     accumulator: isize,
     instruction_pointer: usize,
-    instructions: Vec<Instruction>,
     // used to look for cycles
     executed_instructions: HashSet<usize>,
 }
 
 impl Computer {
-    fn new(instructions: Vec<Instruction>) -> Computer {
+    fn new() -> Computer {
         Computer {
             accumulator: 0,
             instruction_pointer: 0,
-            instructions,
             executed_instructions: Default::default(),
         }
     }
 
-    fn execute_until_loop(&mut self) -> isize {
+    fn reset(&mut self) {
+        self.accumulator = 0;
+        self.instruction_pointer = 0;
+        self.executed_instructions = HashSet::new();
+    }
+
+    fn execute_instruction(&mut self, instruction: &Instruction) {
+        match instruction.opcode {
+            Opcode::Nop => self.instruction_pointer += 1,
+            Opcode::Acc => {
+                self.accumulator += instruction.operand;
+                self.instruction_pointer += 1;
+            }
+            Opcode::Jump => {
+                self.instruction_pointer =
+                    (self.instruction_pointer as isize + instruction.operand) as usize
+            }
+        }
+    }
+
+    fn execute_program(&mut self, program: &[Instruction]) -> Result<isize, ExecutionError> {
         loop {
             if self
                 .executed_instructions
                 .contains(&self.instruction_pointer)
             {
-                return self.accumulator;
+                // we run into an execution loop
+                return Err(ExecutionError::LoopDetected(self.accumulator));
             }
+
+            // we have terminated
+            if self.instruction_pointer == program.len() {
+                return Ok(self.accumulator);
+            }
+
             self.executed_instructions.insert(self.instruction_pointer);
-            let instruction = &self.instructions[self.instruction_pointer];
-            match instruction.opcode {
-                Opcode::Nop => self.instruction_pointer += 1,
-                Opcode::Acc => {
-                    self.accumulator += instruction.operand;
-                    self.instruction_pointer += 1;
-                }
-                Opcode::Jump => {
-                    self.instruction_pointer =
-                        (self.instruction_pointer as isize + instruction.operand) as usize
-                }
-            }
+            let next_instruction = &program[self.instruction_pointer];
+            self.execute_instruction(next_instruction);
         }
+    }
+
+    fn determine_non_halting_set(&mut self, program: &[Instruction]) -> HashSet<usize> {
+        if self.execute_program(program).is_ok() {
+            panic!("program did not loop!")
+        }
+        let non_halting_set = self.executed_instructions.clone();
+        self.reset();
+        non_halting_set
     }
 }
 
 fn parse_as_instructions(raw: &[String]) -> Vec<Instruction> {
-    raw.into_iter()
+    raw.iter()
         .map(|raw_instruction| {
             Instruction::try_from(raw_instruction).expect("failed to parse instruction")
         })
@@ -136,11 +190,46 @@ fn parse_as_instructions(raw: &[String]) -> Vec<Instruction> {
 
 fn part1(input: &[String]) -> isize {
     let instructions = parse_as_instructions(input);
-    Computer::new(instructions).execute_until_loop()
+    match Computer::new().execute_program(&instructions) {
+        Err(err) => err.into_inner(),
+        Ok(_) => panic!("managed to terminate in part1!"),
+    }
 }
 
-fn part2(input: &[String]) -> isize {
-    0
+fn substitute_instruction(instruction: &mut Instruction) -> bool {
+    match instruction.opcode {
+        Opcode::Nop => {
+            instruction.opcode = Opcode::Jump;
+            true
+        }
+        Opcode::Jump => {
+            instruction.opcode = Opcode::Nop;
+            true
+        }
+        _ => false,
+    }
+}
+
+fn part2(input: &[String]) -> Option<isize> {
+    let mut instructions = parse_as_instructions(input);
+    let mut computer = Computer::new();
+    // there is no point in trying to substitute instructions that were never executed in the
+    // looping case
+    let non_halting_set = computer.determine_non_halting_set(&instructions);
+    for non_halting in non_halting_set.iter() {
+        // attempt substitution of any non-halting instructions in the program
+        // but don't run the program if we run into an 'acc'
+        if substitute_instruction(&mut instructions[*non_halting]) {
+            if let Ok(termination_result) = computer.execute_program(&instructions) {
+                return Some(termination_result);
+            } else {
+                computer.reset();
+                substitute_instruction(&mut instructions[*non_halting]);
+            }
+        }
+    }
+
+    None
 }
 
 #[cfg(not(tarpaulin))]
@@ -150,7 +239,7 @@ fn main() {
     let part1_result = part1(&input);
     println!("Part 1 result is {}", part1_result);
 
-    let part2_result = part2(&input);
+    let part2_result = part2(&input).expect("failed to solve part2");
     println!("Part 2 result is {}", part2_result);
 }
 
@@ -175,5 +264,24 @@ mod tests {
         let expected = 5;
 
         assert_eq!(expected, part1(&input))
+    }
+
+    #[test]
+    fn part2_sample_input() {
+        let input = vec![
+            "nop +0".to_string(),
+            "acc +1".to_string(),
+            "jmp +4".to_string(),
+            "acc +3".to_string(),
+            "jmp -3".to_string(),
+            "acc -99".to_string(),
+            "acc +1".to_string(),
+            "jmp -4".to_string(),
+            "acc +6".to_string(),
+        ];
+
+        let expected = 8;
+
+        assert_eq!(expected, part2(&input).unwrap())
     }
 }
