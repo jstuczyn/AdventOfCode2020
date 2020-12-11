3021afbe42423158be731d2b98bfa62499ef8297
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-12-11 12:12:58 +0000

    part 2 done

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -81,36 +81,27 @@ type SeatRow = Vec<Seat>;
 type SeatPosition = (usize, usize);
 
 #[derive(PartialEq)]
-struct SeatGrid(Vec<SeatRow>);
+struct SeatGrid {
+    rows: Vec<SeatRow>,
+}
 
 impl Index<SeatPosition> for SeatGrid {
     type Output = Seat;
 
     fn index(&self, index: SeatPosition) -> &Self::Output {
-        &self.0[index.1][index.0]
+        &self.rows[index.1][index.0]
     }
 }
 
 impl IndexMut<SeatPosition> for SeatGrid {
     fn index_mut(&mut self, index: SeatPosition) -> &mut Self::Output {
-        &mut self.0[index.1][index.0]
-    }
-}
-
-impl From<&[String]> for SeatGrid {
-    fn from(raw_rows: &[String]) -> Self {
-        Self(
-            raw_rows
-                .iter()
-                .map(|row| row.chars().map(Seat::from).collect())
-                .collect(),
-        )
+        &mut self.rows[index.1][index.0]
     }
 }
 
 impl Display for SeatGrid {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        for row in self.0.iter() {
+        for row in self.rows.iter() {
             let row_string: String = row
                 .iter()
                 .map(|&seat| {
@@ -124,8 +115,19 @@ impl Display for SeatGrid {
     }
 }
 
+impl From<&[String]> for SeatGrid {
+    fn from(raw_rows: &[String]) -> Self {
+        Self {
+            rows: raw_rows
+                .iter()
+                .map(|row| row.chars().map(Seat::from).collect())
+                .collect(),
+        }
+    }
+}
+
 impl SeatGrid {
-    fn adjacent_seats(&self, position: SeatPosition) -> Vec<Seat> {
+    fn immediately_adjacent(&self, position: SeatPosition) -> Vec<Seat> {
         let mut adjacent = Vec::new();
         for i in -1..=1 {
             for j in -1..=1 {
@@ -134,6 +136,30 @@ impl SeatGrid {
                 }
                 if let Some(seat) = self.attempt_seat_lookup(position, (i, j)) {
                     adjacent.push(seat)
+                }
+            }
+        }
+
+        adjacent
+    }
+
+    fn visibly_adjacent(&self, position: SeatPosition) -> Vec<Seat> {
+        let mut adjacent = Vec::new();
+
+        for i in -1..=1 {
+            for j in -1..=1 {
+                if i == 0 && j == 0 {
+                    continue;
+                }
+                let mut translation = (i, j);
+                while let Some(seat) = self.attempt_seat_lookup(position, translation) {
+                    if !seat.is_floor() {
+                        adjacent.push(seat);
+                        break;
+                    } else {
+                        translation.0 += i;
+                        translation.1 += j;
+                    }
                 }
             }
         }
@@ -151,9 +177,9 @@ impl SeatGrid {
         let translated = (x as isize + dx, y as isize + dy);
 
         if translated.0 < 0
-            || translated.0 >= self.0[0].len() as isize
+            || translated.0 >= self.rows[0].len() as isize
             || translated.1 < 0
-            || translated.1 >= self.0.len() as isize
+            || translated.1 >= self.rows.len() as isize
         {
             None
         } else {
@@ -161,31 +187,24 @@ impl SeatGrid {
             let new_position = (translated.0 as usize, translated.1 as usize);
 
             Some(self[new_position])
-            // if !seat.is_floor() {
-            //     // we don't care about floor seats (at least in part 1)
-            //     Some(seat)
-            // } else {
-            //     None
-            // }
         }
     }
 
-    fn simulate_step(&self) -> Self {
-        let mut new_grid = SeatGrid(self.0.clone());
+    fn simulate_step<F, C>(&self, adjacent_seats: F, seat_checker: C) -> Self
+    where
+        F: Fn(&SeatGrid, SeatPosition) -> Vec<Seat>,
+        C: Fn(&Seat, &[Seat]) -> bool,
+    {
+        let mut new_grid = SeatGrid {
+            rows: self.rows.clone(),
+        };
 
-        self.0.iter().enumerate().for_each(|(y, row)| {
+        self.rows.iter().enumerate().for_each(|(y, row)| {
             row.iter().enumerate().for_each(|(x, seat)| {
                 if !seat.is_floor() {
-                    let adjacent = self.adjacent_seats((x, y));
+                    let adjacent = adjacent_seats(self, (x, y));
 
-                    // If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
-                    if seat.is_empty() && !adjacent.iter().any(|adj| adj.is_occupied()) {
-                        new_grid[(x, y)].swap();
-                    }
-                    // If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
-                    if seat.is_occupied()
-                        && adjacent.iter().filter(|seat| seat.is_occupied()).count() >= 4
-                    {
+                    if seat_checker(seat, &adjacent) {
                         new_grid[(x, y)].swap();
                     }
                 }
@@ -196,7 +215,7 @@ impl SeatGrid {
     }
 
     fn occupied_count(&self) -> usize {
-        self.0
+        self.rows
             .iter()
             .flat_map(|row| row.iter())
             .filter(|seat| seat.is_occupied())
@@ -205,9 +224,21 @@ impl SeatGrid {
 }
 
 fn part1(input: &[String]) -> usize {
+    let seat_checker = |seat: &Seat, adjacent: &[Seat]| {
+        // If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
+        if seat.is_empty() && !adjacent.iter().any(|adj| adj.is_occupied()) {
+            return true;
+        }
+        // If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
+        if seat.is_occupied() && adjacent.iter().filter(|seat| seat.is_occupied()).count() >= 4 {
+            return true;
+        }
+        false
+    };
+
     let mut grid = SeatGrid::from(input);
     loop {
-        let next_grid = grid.simulate_step();
+        let next_grid = grid.simulate_step(SeatGrid::immediately_adjacent, seat_checker);
         if next_grid == grid {
             break;
         }
@@ -216,9 +247,29 @@ fn part1(input: &[String]) -> usize {
     grid.occupied_count()
 }
 
-// fn part2(input: &[String]) -> usize {
-//     0
-// }
+fn part2(input: &[String]) -> usize {
+    let seat_checker = |seat: &Seat, adjacent: &[Seat]| {
+        // If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
+        if seat.is_empty() && !adjacent.iter().any(|adj| adj.is_occupied()) {
+            return true;
+        }
+        // it now takes five or more visible occupied seats for an occupied seat to become empty (rather than four or more from the previous rules)
+        if seat.is_occupied() && adjacent.iter().filter(|seat| seat.is_occupied()).count() >= 5 {
+            return true;
+        }
+        false
+    };
+
+    let mut grid = SeatGrid::from(input);
+    loop {
+        let next_grid = grid.simulate_step(SeatGrid::visibly_adjacent, seat_checker);
+        if next_grid == grid {
+            break;
+        }
+        grid = next_grid;
+    }
+    grid.occupied_count()
+}
 
 #[cfg(not(tarpaulin))]
 fn main() {
@@ -226,9 +277,9 @@ fn main() {
 
     let part1_result = part1(&input);
     println!("Part 1 result is {}", part1_result);
-    //
-    // let part2_result = part2(&input);
-    // println!("Part 2 result is {}", part2_result);
+
+    let part2_result = part2(&input);
+    println!("Part 2 result is {}", part2_result);
 }
 
 #[cfg(test)]
@@ -253,5 +304,25 @@ mod tests {
         let expected = 37;
 
         assert_eq!(expected, part1(&input))
+    }
+
+    #[test]
+    fn part2_sample_input() {
+        let input = vec![
+            "L.LL.LL.LL".to_string(),
+            "LLLLLLL.LL".to_string(),
+            "L.L.L..L..".to_string(),
+            "LLLL.LL.LL".to_string(),
+            "L.LL.LL.LL".to_string(),
+            "L.LLLLL.LL".to_string(),
+            "..L.L.....".to_string(),
+            "LLLLLLLLLL".to_string(),
+            "L.LLLLLL.L".to_string(),
+            "L.LLLLL.LL".to_string(),
+        ];
+
+        let expected = 26;
+
+        assert_eq!(expected, part2(&input))
     }
 }
