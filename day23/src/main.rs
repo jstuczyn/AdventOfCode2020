30ced59b597372f92e977dc908b5ca6035c150bc
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-12-23 11:23:22 +0000

    day23 init

---
+++ day23/src/main.rs
@@ -0,0 +1,35 @@
+// Copyright 2020 Jedrzej Stuczynski
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+fn part1(input: &[usize]) -> usize {
+    0
+}
+
+// fn part2(input: &[usize]) -> usize {
+// 0
+// }
+
+fn split_into_digits(number: usize) -> Vec<usize> {}
+
+#[cfg(not(tarpaulin))]
+fn main() {
+    let input = 364289715;
+    let split_input = split_into_digits(input);
+
+    let part1_result = part1(&split_input);
+    println!("Part 1 result is {}", part1_result);
+
+    // let part2_result = part2(&input);
+    // println!("Part 2 result is {}", part2_result);
+}
